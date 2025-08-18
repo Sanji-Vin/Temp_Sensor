6b9b7b1776b7fbd755e34160ca38dc8994b46de7
Sanji-Vin <ayoubferjani789@gmail.com>
2025-08-18 15:18:51 +0100

    updated the pubspec and constants folder

---
+++ tempsensor/lib/constants/constant.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const cardBackgroundColor = Color(0x02304733);
+const primaryColor = Color(0xFF219EBC);
+const secondaryColor = Color(0xFF8ECAE6);
+const backgroundColor = Color(0xFFFFB703);
+const selectionColor = Color(0xFFFB8500);
+const defaultPadding = 20.0;
--- tempsensor/lib/main.dart
+++ tempsensor/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:tempsensor/constants/constant.dart';
+import 'package:tempsensor/screens/main_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +11,14 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      title: "Temperature Sensor",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: backgroundColor,
+        brightness: Brightness.dark,
       ),
+      home: const MainScreen(),
     );
   }
 }
