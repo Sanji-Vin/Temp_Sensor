fad4de37ead77e7ac97b8e0207d4b8bbef183130
Sanji-Vin <ayoubferjani789@gmail.com>
2025-08-20 23:03:25 +0100

    added libraries

--- tempsensor/lib/data/health_details.dart
+++ tempsensor/lib/data/health_details.dart
@@ -2,17 +2,17 @@ import 'package:tempsensor/model/health_model.dart';
 
 class HealthDetails {
   final healthData = const [
-    HealthModel(icon: 'assets/icons/burn.png', value: '305', title: "temp"),
+    HealthModel(icon: 'assets/icons/burn.png', value: '30°C', title: "temp"),
     HealthModel(
       icon: 'assets/icons/humidity.png',
-      value: '305',
+      value: '20',
       title: "humidity",
     ),
     HealthModel(
       icon: 'assets/icons/aq.png',
-      value: '305',
+      value: '40',
       title: "air_Quality",
     ),
-    HealthModel(icon: 'assets/icons/light.png', value: '305', title: "Light"),
+    HealthModel(icon: 'assets/icons/light.png', value: '400', title: "Light"),
   ];
 }
